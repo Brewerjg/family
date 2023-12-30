6132dc9bae17fa8526367056e014249fed0341aa
Brewerjg <grammerjasonl@gmail.com>
2023-12-29 22:36:29 -0600

    google ver

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './views/home';
 import Detail from './views/detail';
+import Ver from './views/family/client/src/views/google46f2d02cdf588407.html'
 
 
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route element={<Main/>} path="/" default/>  
                 <Route element={<Detail/>} path="/image/:id"/> 
+                <Route element={<Ver/>} path="/google46f2d02cdf588407.html"/>
             </Routes>
     	</BrowserRouter>
     </div>
--- client/src/components/create.jsx
+++ client/src/components/create.jsx
@@ -36,7 +36,7 @@ export default function Create () {
           
         }
         createImg(image);
-        navigate('/');
+        
       };
   
     return (
