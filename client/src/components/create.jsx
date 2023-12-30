63f3c439ef1af46b64211096bdf9628ef4d6e437
Brewerjg <grammerjasonl@gmail.com>
2023-12-30 11:53:52 -0600

    refactored create form

--- client/src/components/create.jsx
+++ client/src/components/create.jsx
@@ -3,71 +3,69 @@ import axios from 'axios';
 import FileBase64 from 'react-file-base64';
 import { useNavigate } from 'react-router-dom';
 
+export default function Create() {
+  const [image, setImage] = useState({
+    title: "",
+    imageBody: "",
+    image: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
+  const url = "http://localhost:8000/api/image/new";
+  const createImage = async (newImage) => {
+    try {
+      await axios.post(url, newImage);
+      alert("Image Added");
+    } catch (error) {
+      console.log(error.message);
+      alert("Invalid Form, Please Try Again");
+    }
+  };
 
-export default function Create () {
-    const [image , setImage] = useState({
-        title: "",
-        imageBody: "",
-        image: "",
-    });
-    const error = useState("");
-    const navigate = useNavigate();
-    
-    const url = "http://localhost:8000/api/image/new";
-    const createImage = (newImage) => axios.post(url, newImage);
-    
-      const createImg = async (image) => {
-        try {
-          await createImage(image);
-          return alert("Image Added");
-          
-        } catch (setError) {
-          console.log(error.message);
-          return alert("Invalid Form, Please Try Again");
-        }
-      };
+  const handleSubmit = () => {
+    if (image.image === "" || image.image === null) {
+      alert("Please Upload an Image");
+    } else {
+      createImage(image);
+      navigate("/");
+    }
+  };
 
-      const handleSubmit = () => {
-        if (image.image === "" || image.image === null) {
-          return alert("Please Upload an Image");
-          
-        }
-        createImg(image);
-        navigate("/");
-      };
-  
-    return (
+  const handleInputChange = (e) => {
+    setImage({ ...image, [e.target.name]: e.target.value });
+  };
+
+  return (
     <div className='flex justify-center'>
       <div className='flex flex-col bg-gray-600 rounded-2xl m-10 h-1/3 w-1/3 p-5 ' >
         <div className='flex justify-center'>
           <h1 className='text-5xl text-red-900 m-4'>Add Image</h1>
         </div>
         <form onSubmit={handleSubmit}>
-        
           <div>
-          <label className='text-white-900' for="title">Choose a name:</label>
-                    <select className='w-2/3 rounded-xl' name="title" id="title" onChange={e => setImage({ ...image, title: e.target.value })} path={image.title}>
-                      <option  value="">--Please choose an option--</option>
-                      <option value="Grammer">Grammer</option>
-                      <option value="Bhakta">Bhakta</option>
-                      <option value="Hooks">Hooks</option>
-                    </select>
+            <label className='text-white-900' htmlFor="title">Choose a name:</label>
+            <select className='w-2/3 rounded-xl' name="title" id="title" onChange={handleInputChange} value={image.title}>
+              <option value="">--Please choose an option--</option>
+              <option value="Grammer">Grammer</option>
+              <option value="Bhakta">Bhakta</option>
+              <option value="Hooks">Hooks</option>
+            </select>
           </div>
           <div className="form-group m-2">
-            <label path="imageBody">Description:</label>
-              <input required="" className="form-control" onChange={e => setImage({ ...image, imageBody: e.target.value })} path={image.imageBody} name="imageBody" id="imageBody" type="text"/>
+            <label htmlFor="imageBody">Description:</label>
+            <input required className="form-control" onChange={handleInputChange} value={image.imageBody} name="imageBody" id="imageBody" type="text"/>
           </div>
           <FileBase64 
-          type="file"
-          multiple={false}
-          required={true}
-          onDone={({ base64 }) => setImage({ ...image, image: base64 })}
+            type="file"
+            multiple={false}
+            required={true}
+            onDone={({ base64 }) => setImage({ ...image, image: base64 })}
           />
           <button className='btn'>Submit</button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
