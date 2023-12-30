ff438596ce162e91acfa98b9a10f5f50c6acd4d8
Brewerjg <grammerjasonl@gmail.com>
2023-12-29 23:33:48 -0600

    revert

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Navbar from '../components/navbar';
 import DisplayImg from '../components/displayImg';
 import { useState } from 'react';
-import Create from '../components/create';
-import Carousel from '../components/EmblaCarousel';
+
 
 
 const Home = () => {
@@ -15,8 +14,6 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
-      <Create/>
-      <Carousel/>
       <DisplayImg image={image} setImage={setImage} removeDom={removeDom}/>
     </div>
   )
